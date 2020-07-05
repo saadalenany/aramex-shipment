26216619c582af7a850f5de0752fd94f35ff8d4a
saadalenany <saad.alenany@yahoo.com>
2020-07-05 05:18:02 +0200

    Setting up tests...

---
+++ src/test/java/com/nasnav/controller/DataProcessingControllerTest.java
@@ -0,0 +1,16 @@
+package com.nasnav.controller;
+
+import com.nasnav.BaseTest;
+import org.junit.Test;
+
+public class DataProcessingControllerTest extends BaseTest {
+
+    private final String upload = "/dataprocess/loadxls";
+
+    private final String download = "/dataprocess/getcsv";
+
+    @Test
+    public void testUploadXls() {
+
+    }
+}
