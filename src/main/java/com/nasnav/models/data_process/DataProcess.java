e5fbcd4643333ce97bf0d693ee7efc20a19541ce
saadalenany <saad.alenany@yahoo.com>
2020-07-08 18:21:59 +0200

    Updating Dataprocess pojo

--- src/main/java/com/nasnav/models/data_process/DataProcess.java
+++ src/main/java/com/nasnav/models/data_process/DataProcess.java
@@ -7,48 +7,48 @@ import lombok.Data;
 @Data
 public class DataProcess {
 
-    @ExcelCellName("quantity")
-    @CsvBindByName(column = "quantity")
+    @ExcelCellName("Quantity")
+    @CsvBindByName(column = "Quantity")
     private Integer quantity;
 
-    @ExcelCellName("variant_id")
-    @CsvBindByName(column = "variant_id")
+    @ExcelCellName("Variant Id")
+    @CsvBindByName(column = "Variant Id")
     private String variantId;
 
-    @ExcelCellName("price")
-    @CsvBindByName(column = "price")
+    @ExcelCellName("Price")
+    @CsvBindByName(column = "Price")
     private Double price;
 
-    @ExcelCellName("description")
-    @CsvBindByName(column = "description")
+    @ExcelCellName("Description")
+    @CsvBindByName(column = "Description")
     private String description;
 
-    @ExcelCellName("discount")
-    @CsvBindByName(column = "discount")
+    @ExcelCellName("Discount")
+    @CsvBindByName(column = "Discount")
     private Double discount;
 
-    @ExcelCellName("external_id")
-    @CsvBindByName(column = "external_id")
+    @ExcelCellName("External Id")
+    @CsvBindByName(column = "External Id")
     private String externalId;
 
-    @ExcelCellName("product_group_key")
-    @CsvBindByName(column = "product_group_key")
+    @ExcelCellName("Product Group Key")
+    @CsvBindByName(column = "Product Group Key")
     private String productGroupKey;
 
-    @ExcelCellName("product_name")
-    @CsvBindByName(column = "product_name")
+    @ExcelCellName("Product Name")
+    @CsvBindByName(column = "Product Name")
     private String productName;
 
-    @ExcelCellName("barcode")
-    @CsvBindByName(column = "barcode")
+    @ExcelCellName("Bar Code")
+    @CsvBindByName(column = "Bar Code")
     private String barcode;
 
-    @ExcelCellName("brand")
-    @CsvBindByName(column = "brand")
+    @ExcelCellName("Brand")
+    @CsvBindByName(column = "Brand")
     private String brand;
 
-    @ExcelCellName("tags")
-    @CsvBindByName(column = "tags")
+    @ExcelCellName("Tags")
+    @CsvBindByName(column = "Tags")
     private String tags;
 
     @ExcelCellName("Class")
@@ -106,4 +106,149 @@ public class DataProcess {
     @ExcelCellName("LEVI'S CODE")
     @CsvBindByName(column = "LEVI'S CODE")
     private String levisCode;
+
+    @ExcelCellName("Lookup Code")
+    @CsvBindByName(column = "Lookup Code")
+    private String lookupCode;
+
+    @ExcelCellName("ITEM CODE (Rasport)")
+    @CsvBindByName(column = "ITEM CODE (Rasport)")
+    private String itemCode;
+
+    @ExcelCellName("By Model")
+    @CsvBindByName(column = "By Model")
+    private String byModel;
+
+    @ExcelCellName("Style Name")
+    @CsvBindByName(column = "Style Name")
+    private String styleName;
+
+    @ExcelCellName("Color Family")
+    @CsvBindByName(column = "Color Family")
+    private String colorFamily;
+
+    @ExcelCellName("Division")
+    @CsvBindByName(column = "Division")
+    private String division;
+
+    @ExcelCellName("Gender")
+    @CsvBindByName(column = "Gender")
+    private String gender;
+
+    @ExcelCellName("Category")
+    @CsvBindByName(column = "Category")
+    private String category;
+
+    @ExcelCellName("Class Hierarchy")
+    @CsvBindByName(column = "Class Hierarchy")
+    private String classHierarchy;
+
+    @ExcelCellName("Fit Name")
+    @CsvBindByName(column = "Fit Name")
+    private String fitName;
+
+    @ExcelCellName("Sub Class Hierarchy")
+    @CsvBindByName(column = "Sub Class Hierarchy")
+    private String subClassHierarchy;
+
+    @ExcelCellName("Leg Opening")
+    @CsvBindByName(column = "Leg Opening")
+    private String legOpening;
+
+    @ExcelCellName("Closure/Collar")
+    @CsvBindByName(column = "Closure/Collar")
+    private String closure;
+
+    @ExcelCellName("Pocket Styling")
+    @CsvBindByName(column = "Pocket Styling")
+    private String pocketStyling;
+
+    @ExcelCellName("Misc. Features & Benefits")
+    @CsvBindByName(column = "Misc. Features & Benefits")
+    private String miscFeaturesBenefits;
+
+    @ExcelCellName("Wash Instructions")
+    @CsvBindByName(column = "Wash Instructions")
+    private String washInstructions;
+
+    @ExcelCellName("Class Hierarchy2")
+    @CsvBindByName(column = "Class Hierarchy2")
+    private String classHierarchy2;
+
+    @ExcelCellName("Sub Class Hierarchy2")
+    @CsvBindByName(column = "Sub Class Hierarchy2")
+    private String subClassHierarchy2;
+
+    @ExcelCellName("Seasonality")
+    @CsvBindByName(column = "Seasonality")
+    private String seasonality;
+
+    @ExcelCellName("EAN Code")
+    @CsvBindByName(column = "EAN Code")
+    private String eanCode;
+
+    @ExcelCellName("Fabrication")
+    @CsvBindByName(column = "Fabrication")
+    private String fabrication;
+
+    @ExcelCellName("Levi's® Collaboration")
+    @CsvBindByName(column = "Levi's® Collaboration")
+    private String levisCollaboration;
+
+    @ExcelCellName("Image")
+    @CsvBindByName(column = "Image")
+    private String image;
+
+    @ExcelCellName("Link")
+    @CsvBindByName(column = "Link")
+    private String link;
+
+    @ExcelCellName("In store")
+    @CsvBindByName(column = "In store")
+    private String inStore;
+
+    @ExcelCellName("Remarks")
+    @CsvBindByName(column = "Remarks")
+    private String remarks;
+
+    @ExcelCellName("Action")
+    @CsvBindByName(column = "Action")
+    private String action;
+
+    @ExcelCellName("Article No")
+    @CsvBindByName(column = "Article No")
+    private String articleNo;
+
+    @ExcelCellName("Family")
+    @CsvBindByName(column = "Family")
+    private String family;
+
+    @ExcelCellName("Art Name")
+    @CsvBindByName(column = "Art Name")
+    private String artName;
+
+    @ExcelCellName("Season")
+    @CsvBindByName(column = "Season")
+    private String season;
+
+    @ExcelCellName("Product Group")
+    @CsvBindByName(column = "Product Group")
+    private String productGroup;
+
+    @ExcelCellName("Product Type")
+    @CsvBindByName(column = "Product Type")
+    private String productType;
+
+    @ExcelCellName("SUBGender")
+    @CsvBindByName(column = "SUBGender")
+    private String subGender;
+
+    @ExcelCellName("Sex")
+    @CsvBindByName(column = "Sex")
+    private String sex;
+
+    @ExcelCellName("RTP")
+    @CsvBindByName(column = "RTP")
+    private String rtp;
+
 }
